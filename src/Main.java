cd125db35a3d775398340956246afc2ed015096e
EdwinSarango12 <edwin.sarango@epn.edu.ec>
2024-10-05 20:13:29 -0500

    Codigo correjido, siguiendo las instrucciones del deber

--- src/Main.java
+++ src/Main.java
@@ -3,51 +3,56 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-
-        //Dany esto es parte del menu si crees poder mejorarlo ahi le mueves de una :D
-
-        System.out.println("Aquí escribir el codigo :)");
         Scanner sc = new Scanner(System.in);
-        System.out.println("-----Calculadora-----");
-        System.out.printf("\t MENU \t");
-        System.out.printf("1. Sumar");
-        System.out.printf("2. Restar");
-        System.out.printf("3. Multiplicar");
-        System.out.printf("4. Division");
-        System.out.println("Seleccionar una opcion:");
-        int opcion = sc.nextInt();
 
-        //introducir los numeros
-        System.out.printf("Introduce el primer numero: ");
-        Double num1 = sc.nextDouble();
-        System.out.printf("Introduce el segundo numero: ");
-        Double num2 = sc.nextDouble();
+        // Creación del menu
 
-        double resultado =0;
+        int opcion;
+        boolean continuar = true;
+        while (continuar) {
 
-        // lógica
+            // Si quieres mejorar el menu de una :D
 
-        switch (opcion){
-            case 1:
-                resultado = num1 + num2;
-                System.out.println("El resultado es: " + resultado);
-                break;
-            case 2:
-                resultado = num1 - num2;
-                System.out.println("El resultado es: " + resultado);
-                break;
-            case 3:
-                resultado = num1 * num2;
-                System.out.println("El resultado es: " + resultado);
-                break;
-            case 4:
+            System.out.printf("\tBienvenido al sistema del programa\t");
+            System.out.printf("------Calculadora de Areas------------");
+            System.out.printf("1. Area de un triangulo");
+            System.out.printf("2. Area de un cuadrado");
+            System.out.printf("3. Area de un circulo\n");
+            System.out.println("4. Salir");
+            System.out.println("Seleccione una opcion: ");
+            opcion = sc.nextInt();
 
-                //dany date haciendo la logica de esta parte tengo que salir
-                resultado = num1 / num2;
-                System.out.printf("El resultado es: " + resultado);
-                break;
+            switch (opcion) {
+                case 1:
+                    System.out.println("Introduce la base del triangulo:");
+                    double base = sc.nextDouble();
+                    System.out.println("Introduce la altura del triangulo:");
+                    double altura = sc.nextDouble();
+                    double area = (base*altura)/2;
+                    System.out.println("El area del triangulo es: " + area);
+                    break;
+                case 2:
+                    System.out.println("Introduce el lado del cuadrado:");
+                    double lado = sc.nextDouble();
+                    double cuadrado = lado*lado;
+                    System.out.println("El area del cuadrado es: " + cuadrado);
+                    break;
+                case 3:
+                    System.out.println("Introduce el radio del circulo:");
+                    double radio = sc.nextDouble();
+                    double circulo = Math.PI * Math.pow(radio, 2);
+                    System.out.println("El area del circulo es: " + circulo);
+                    break;
+                case 4:
 
+                    //// Dany ponle la función para salir del programa porfa que ya llego a mi casa :D
+
+
+                    //// Agrega la funcion de la opcion no válida y queda el programa
+                    
+
+
+            }
         }
     }
 }
